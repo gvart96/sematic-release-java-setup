5297b39e9d32d362c4fcdd1f7acc83ce2838d65c
Vladlen Gladis <vladlen.gladis@sellics.com>
2021-12-18 15:38:18 +0100

    feat: init setup

---
+++ src/main/kotlin/md/gvart/semanticreleasejavasetup/SemanticReleaseJavaSetupApplication.kt
@@ -0,0 +1,11 @@
+package md.gvart.semanticreleasejavasetup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SemanticReleaseJavaSetupApplication
+
+fun main(args: Array<String>) {
+    runApplication<SemanticReleaseJavaSetupApplication>(*args)
+}
---
+++ src/test/kotlin/md/gvart/semanticreleasejavasetup/SemanticReleaseJavaSetupApplicationTests.kt
@@ -0,0 +1,13 @@
+package md.gvart.semanticreleasejavasetup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SemanticReleaseJavaSetupApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
